d0b45a3984a7951b6e15b7cd326397f16841dc57
michael <mnzube@gmail.com>
2020-07-12 19:17:14 +0200

    remove gitignore files

---
+++ controllers/profile.js
@@ -0,0 +1,13 @@
+const handleProfile = (req,res,db)=> {
+    const {id} = req.params;
+db.select('*').from('users').where({id}).then(user => {
+   if(user.length){res.json(user[0])} else {
+       res.status(404).json('user not found')
+   }
+})
+.catch(error => res.status(404).json('error getting user'))
+}
+
+module.exports = {
+    handleProfile 
+}
