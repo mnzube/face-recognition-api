222f160f6cc98642fd2a6672df301520e777aab5
michael <mnzube@gmail.com>
2020-07-12 23:03:09 +0200

    updating database connection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client:'pg',
     connection: {
-        host:'postgresql-objective-03044',
-        user:'michaelnzube',
-        password: '',
-        database:'face-recognition'
+        host: process.env.DATABASE_URL,
+        ssl:true
     }
 });
 
