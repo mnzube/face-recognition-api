627e3d95ad8b6347d3768aa55d6e84f38f233782
michael <mnzube@gmail.com>
2020-07-13 11:33:22 +0200

    pg connection

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/', (req,res) => {res.send('working')})
+app.get('/', (req,res) => {res.send('App is working')})
 app.post('/signin', (req,res) => {(signin.handleSignin(req,res,db,bcrypt))}) 
 app.post('/register', (req,res) => {(register.handleRegister(req,res,db,bcrypt))})
 app.get('/profile/:id', (req,res) => {profile.handleProfile(req,res,db)})
